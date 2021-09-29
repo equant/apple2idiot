d18f8a0cc13e643fd9a4ff6121410b4ddf7659fb
equant <nathanhendler@gmail.com>
2021-09-28 21:43:23 -0700

    Regression of ESP32 slack code due to memory corruption.  Try again later.  On to chess!

--- arduino-lib/Apple2Idiot/A2I_commands.h
+++ arduino-lib/Apple2Idiot/A2I_commands.h
@@ -9,10 +9,31 @@
 // If the values of these defines are changed, then they will need to be
 // updated within the code that runs on the Apple as well.
 
-//#define COMMAND_GENERIC 0b10000000
-#define COMMAND_WIFI_SCAN 111
-#define COMMAND_WIFI_CONNECT 112
+#define SHARED_RAM_START_ADDRESS 2
+
 #define ACK 0b00000110  // Acknowledge
 #define EOT 0b00000100  // End of transmit
 #define ERR 0b00000101  // Error
 #define MORE_TO_SEND 0b00000111
+
+/*################################################
+#                GENERIC STATUSES                #
+################################################*/
+#define NOT_FOUND 11
+
+/*################################################
+#                      WIFI                      #
+################################################*/
+#define COMMAND_WIFI_SCAN 111
+#define COMMAND_WIFI_CONNECT 112
+
+/*################################################
+#                     SLACK (50)                 #
+################################################*/
+#define COMMAND_GET_N_CHANNELS 50   // Return number of channels
+#define COMMAND_GET_CHANNEL_N 51    // Return info for channel identified by #n
+#define COMMAND_GET_CHANNEL_STR 52    // Return info for channel identified by string (id or name)
+#define COMMAND_SET_CHANNEL 53
+#define COMMAND_SEND_MESSAGE 54
+#define COMMAND_GET_MESSAGES 55
+
